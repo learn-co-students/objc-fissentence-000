dee8dc662e26f53e8a679602473f47047225f987
Raskovan <alexey.katalkin@gmail.com>
2016-10-25 16:15:12 -0400

    Sentence

---
+++ objc-fissentence/FISSentence.m
@@ -0,0 +1,13 @@
+//
+//  FISSentence.m
+//  objc-fissentence
+//
+//  Created by Katala on 10/25/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISSentence.h"
+
+@implementation FISSentence
+
+@end
